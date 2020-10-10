6c51fda76f43b528c52795a4fe3c8685a5d6ab73
Sebastian Borquez <sebastian.borquez.g@gmail.com>
2020-10-10 12:12:45 -0300

    classify an image

--- main.py
+++ main.py
@@ -1,60 +1,100 @@
-from flask import Flask, render_template, request
-from keras.preprocessing.image import img_to_array
-from keras.models import load_model
+from flask import Flask, render_template, request, jsonify
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
 import cv2
 import numpy as np
 
 from flask_cors import CORS, cross_origin
 
-names = ["daisy", "dandelon", "roses", "sunflowers", "tulips"]
 
+
+def get_model(model_name, get_labels=False):
+    models = {
+        "flower": {
+            "path": "flower.model",
+            "label_names": ["daisy", "dandelon", "roses", "sunflowers", "tulips"]
+        }
+    }
+    model_path = models[model_name]["path"]
+    model = load_model(model_path)
+    if get_labels:
+        model_labels = models[model_name]["label_names"]
+        return model, model_labels
+    return model
 
 
 # Process image and predict label
-def processImg(IMG_PATH):
+def predict(model_name, image_path):
     # Read image
-    model = load_model("flower.model")
-    
     # Preprocess image
-    image = cv2.imread(IMG_PATH)
+    print(image_path)
+    image = cv2.imread(image_path)
     image = cv2.resize(image, (199, 199))
     image = image.astype("float") / 255.0
     image = img_to_array(image)
     image = np.expand_dims(image, axis=0)
 
+    model, model_labels = get_model(model_name, get_labels=True)
     res = model.predict(image)
     label = np.argmax(res)
     print("Label", label)
-    labelName = names[label]
-    print("Label name:", labelName)
-    return labelName
+    label_name = model_labels[label]
+    print("Label name:", label_name)
+    prediction = {
+        "label": int(label),
+        "label_name": label_name
+    }
+    print("prediction:", prediction)
+    return prediction
 
 
 # Initializing flask application
+SECRET_KEY = 'you-will-never-guess'
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 cors = CORS(app)
 
-@app.route("/")
-def main():
+@app.route("/ok")
+def ok():
     return """
         Application is working
     """
 
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 # About page with render template
 @app.route("/about")
-def postsPage():
+def about():
     return render_template("about.html")
 
 # Process images
-@app.route("/process", methods=["POST"])
-def processReq():
-    data = request.files["img"]
-    data.save("img.jpg")
+class UploadForm(FlaskForm):
+    photo = FileField()#validators=[FileAllowed(photos, 'Image only!'), FileRequired('File was empty!')])
+    submit = SubmitField('Upload')
 
-    resp = processImg("img.jpg")
+@app.route("/predict", methods=["POST", "GET"])
+def process_predict():
+    error = ""
+    form = form = UploadForm()
 
-
-    return resp
+    if request.method == 'POST' and 'photo' in request.files:
+        image = request.files["photo"]
+        model_name = request.files.get("model", 'flower')
+        image.save("img.jpg")
+        print(model_name)
+        resp = predict(model_name, "img.jpg")
+        resp = jsonify(resp)
+        resp.status_code = 200
+        print(f"resp:", resp)
+        return resp
+    else:
+        return render_template('predict.html', form=form, message=error)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -2,12 +2,30 @@ import requests
 import time
 
 '''
+TODO: Include this test in base 
+
 your localhost url. If running on port 5000
 '''
-url = "http://localhost:5000/process"
-# Path to image file
-filess = {"img": open("PATH_TO_INPUT_IMAGE", "rb")}
-starttime = time.time()
-results = requests.post(url, files=filess)
-print("time taken:", time.time() - starttime)
-print(results.text)
+
+BASEURL = "http://127.0.0.1:5000/"
+URL_PREDICT = f"{BASEURL}predict"
+IMAGE_PATH = "test_image.jpg"
+
+def test_ok():
+    print("runing ok")
+    starttime = time.time()
+    results = requests.get(BASEURL)
+    print("time taken:", time.time() - starttime)
+    print(results.text)
+
+def test_predict_default(url=URL_PREDICT, image_path=IMAGE_PATH, model='flowers'):
+    # Path to image file
+    files = {"image": open(image_path, "rb"), "model":model}
+    starttime = time.time()
+    # TODO: include model in request
+    results = requests.post(url, files=files)
+    print("time taken:", time.time() - starttime)
+    print(results.text)
+
+def test_predict_custom():
+    test_predict_default(url=URL_PREDICT, image_path=IMAGE_PATH, model='flowers')
